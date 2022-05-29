451721956be4cf75cf681c0d14da22a7b7e28a49
firsovviktor <72372722+firsovviktor@users.noreply.github.com>
2022-05-29 14:48:30 +0300

    counting through 16b with comments

---
+++ main.py
@@ -0,0 +1,197 @@
+import vtk
+from math import sin, cos, pi, sqrt, exp
+import numpy as np
+import quadpy
+from scipy.integrate import quad
+from matplotlib import pyplot as plt
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
+
+def complex_projection(v1, v2):
+    a = np.array([v1.real, v1.imag]).reshape(1, 2)[0]
+    b = np.array([v2.real, v2.imag]).reshape(1, 2)[0]
+    a_on_b = (np.dot(a, b)/np.dot(b, b)) * b
+    return np.absolute(a_on_b[0] + 1j * a_on_b[1])
+
+
+class Field():
+    
+    def __init__(self, U0, N, L, l, R, x, y):
+    
+        self.U0 = U0            # Wave amplitude
+        self.N = N              # The number of angles
+        self.L = L              # Distance from aperture to screen
+        self.k = 2 * pi/l       # Wave number
+        self.R = R              # Radius of the circumscribed circle
+        
+        self.len = 2*R/sin(pi/N)
+        self.A = np.zeros((N+1, 2), dtype=np.complex128)     # Coordinates of vertexes
+        self.S = np.zeros((x+1, y+1), dtype=np.complex128)   # Coordinates of screen mesh
+        self.E = np.zeros((x+1, y+1), dtype=np.complex128)      # Distribution of the field 
+                                                             # on the screen
+        self.I = np.zeros((x+1, y+1), dtype=np.float64)      # Distribution of the 
+                                                             # intensity on the screen
+                                                    
+        self.x = x              # Amount of horizontal steps
+        self.y = y              # Amount of vertical steps
+        self.a = 2 * R          # Side of the screen
+        
+    def make_mesh(self):
+        
+        dx = self.a/self.x
+        dy = self.a/self.y
+        for i in range(self.y+1):
+            y =  self.a/2 - dy*i
+            for j in range(self.x+1):
+                x = -self.a/2 + dx*j
+                self.S[i][j] = x + 1j * y
+    
+    def make_aperture(self):
+    
+        phi = 2*pi/self.N
+        for n in range(self.N):
+            self.A[n][0] = self.R * cos(n * phi)
+            self.A[n][1] = self.R * sin(n * phi)
+        self.A[self.N] = self.A[0]
+        self.A = np.apply_along_axis(lambda args: [complex(*args)], 1, self.A)
+    
+    def print_aperture(self):
+    
+        X = self.S.real
+        Y = self.S.imag
+        x = self.A.real
+        y = self.A.imag
+        
+        fig, axs = plt.subplots()
+        axs.set_aspect('equal', 'box')
+        plt.plot(x, y)
+        plt.scatter(X, Y)
+        plt.grid()
+        plt.show()  
+
+    
+    def make_integral_sum(self, x, y):
+        p = self.S[x][y]
+        summ = 0
+        
+        for j in range(self.N):
+            qj  = self.A[j]
+            qj_ = self.A[j+1]
+            Lj  = qj_ - qj
+            tj  = Lj/np.absolute(Lj)
+            t0  = complex_projection((qj - p), tj)
+            t1  = (np.absolute(Lj) + t0)[0]
+            #print(np.absolute(qj - p)**2 - t0**2)
+            #fix
+            nj = (qj_ - qj)*(-1*1j)/np.abs(Lj)
+            a = (qj - p).real * nj.real + (qj-p).imag * nj.imag
+            #fix
+
+            #old# a = sqrt(abs(np.absolute(qj - p)**2 - t0**2))
+            b = self.k/(2*self.L)
+            if (a!=0):
+                I, err = (quadpy.quad(lambda x: np.exp(1j * b * x**2) / 
+                         (x**2 + abs(a)**2), t0, t1, epsabs=100, epsrel=100, limit=495))
+                I_ = a * np.exp(1j * b * abs(a)**2) * I
+                summ += I_
+            else:
+                summ += 0
+
+            if (True):
+              eps = 1e-4
+              x = p.real
+              y = p.imag
+              if (np.abs(x)<eps):
+                if (np.abs(y)<eps):
+                  print (I_, a)
+        if (False):
+          eps = 1e-4
+          x = p.real
+          y = p.imag
+          if (np.abs(x)<eps):
+            if (np.abs(y)<eps):
+              print()
+        return(summ)
+    
+    
+    def calculate_E(self):
+        pol = []
+        for i in range(self.N):
+            pol.append((self.A[i].real[0], self.A[i].imag[0]))
+        #print(pol)
+        polygon = Polygon(pol)
+        for i in range(self.y+1):
+            for j in range(self.x+1):             
+                if (True):
+                  eps = 1e-4
+                  x = self.S[i][j].real
+                  y = self.S[i][j].imag
+                  if (np.abs(x)<eps):
+                    if (np.abs(y)<eps):
+                      print (x, y)
+
+                I = self.make_integral_sum(i, j)
+                point = Point(self.S[i][j].real, self.S[i][j].imag)
+                if (polygon.contains(point)):
+                    e = 1
+                else:
+                    e = 0
+                self.E[i][j] += self.U0 * (e - 1/(2*pi) * I)
+
+                if (True):
+                  eps = 1e-4
+                  x = self.S[i][j].real
+                  y = self.S[i][j].imag
+                  if (np.abs(x)<eps):
+                    if (np.abs(y)<eps):
+                      #print (x, y)
+                      print ("I", I)
+                      print ()
+    
+    def calculate_I(self): 
+    
+        for i in range(self.y+1):
+            for j in range(self.x+1):
+                self.I[i][j] = self.E[i][j] * np.conj(self.E[i][j]) / self.U0**2 
+                
+    def snapshot(self):
+    
+        structuredGrid = vtk.vtkStructuredGrid()
+        points = vtk.vtkPoints()
+        E = vtk.vtkDoubleArray()
+        E.SetName("E")
+        
+        for i in range(self.y+1):
+            for j in range(self.x+1):
+                points.InsertNextPoint(self.S[i][j].real, self.S[i][j].imag, 0)
+                E.InsertNextValue(self.I[i][j])
+                
+                if (False):
+                    eps = 1e-4
+                    if (np.abs(self.S[i][j].real)<eps):
+                        if (np.abs(self.S[i][j].imag)<eps):
+                            print (self.S[i][j].real, self.S[i][j].imag, self.E[i][j], self.I[i][j])
+        
+        structuredGrid.SetDimensions(self.x+1, self.y+1, 1)
+        structuredGrid.SetPoints(points)
+
+        structuredGrid.GetPointData().AddArray(E)
+        
+        writer = vtk.vtkXMLStructuredGridWriter()
+        writer.SetInputDataObject(structuredGrid)
+        writer.SetFileName("diff.vts")
+        writer.Write()
+        #U0, N, L,   l,         R,     x,   y  
+f = Field(1, 3, 0.5, 0.0000007, 0.001, 50, 50)
+f.make_aperture()
+
+if (True):
+  print ("hole")
+  for i in range(f.A.size - 1):
+    print("vert", i, np.real(f.A[i]), np.imag(f.A[i]))
+
+f.make_mesh()             
+#f.print_aperture()
+f.calculate_E()
+f.calculate_I()
+f.snapshot()
